e6920bbe72e82a1f7142d862bc9e0efe4ef1862e
alber <alber@ALBERTO_BARBOSA>
2022-08-12 09:28:31 -0300

    Adicionar arquivos de projeto.

---
+++ MaiorNumero/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MaiorNumero;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int N1, N2;
+
+        Console.WriteLine("Bom dia, vamos ver qual dos numeros digotas é o maior.");
+        Console.Write("Digite por favor um numero inteiro = ");
+        N1 = int.Parse(Console.ReadLine());
+        Console.Write("Digite por favor outro numero inteiro = ");
+        N2 = int.Parse(Console.ReadLine());
+
+        if(N1 > N2)
+        {
+            Console.WriteLine("Maior numero digitado é = " + N1);
+        }
+        else if(N1 < N2)
+        {
+            Console.WriteLine("Maior numero digitado é = " + N2);
+        }
+    }
+}
